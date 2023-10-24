c84293ac50bae6b48f753298412287a14ffb072b
ved-thakre <vedantthakre7@gmail.com>
2023-10-24 22:32:22 +0530

    final commit

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -3,7 +3,7 @@ const Social = () => {
   return (
     <div className="home__social">
       <a
-        href="https://www.instagram.com"
+        href="https://www.twitter.com"
         className="home__social-icon"
         target="_black"
       >
