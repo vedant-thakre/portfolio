12dd29bc1c9a317939cdb13e60e37175db63dd44
ved-thakre <vedantthakre7@gmail.com>
2023-10-28 22:39:28 +0530

    few more changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,19 +9,19 @@ const Footer = () => {
 
         <ul className="footer__list">
           <li>
-            <a href="#about" className="footer__link">
+            <a href="#home" className="footer__link">
               Home
             </a>
           </li>
 
           <li>
-            <a href="#projects" className="footer__link">
+            <a href="#about" className="footer__link">
               About
             </a>
           </li>
 
           <li>
-            <a href="#home" className="footer__link">
+            <a href="#portfolio" className="footer__link">
               Projects
             </a>
           </li>
